30218dcce695a803965cbb17dcea172dfbdb3564
aquiya <96477828+dssskvzzz@users.noreply.github.com>
2024-07-07 04:05:49 +0300

    Creating file

---
+++ menu/__init__.py
@@ -0,0 +1,5 @@
+from .main_menu import main_menu
+from .display_info import display_info
+from .display_repositories import display_repositories
+from .display_projects import display_projects
+from .display_settings import display_settings
